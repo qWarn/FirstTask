6b0ee8d1849022397c11a57c653da1739ee7c4db
Nikita Kirilov <nkirilov7even@gmail.com>
2024-07-04 23:35:40 +0300

    update Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.example.entities.File;
+import org.example.utils.IOUtils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static final String PATH_TO_ROOT_FOLDER = "C:\\firstTask";
+    public static final String PATH_TO_RESULT_FILE = "C:\\result\\res.txt";
+
+    public static final String FILE_EXTENSION = ".txt";
+
+    public static void main(String[] args) throws IOException {
+        List<File> files = new ArrayList<>();
+
+        IOUtils.readFilesFromFolder(files);
+        IOUtils.writeResultToFile(files);
     }
 }
